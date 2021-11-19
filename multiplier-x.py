5799228058df09d73bbace404078a3982395ca50
LarsVDN <20481220+LarsVDN@users.noreply.github.com>
2021-11-19 11:34:56 +0100

    stuff

--- multiplier-x.py
+++ multiplier-x.py
@@ -1,9 +1,9 @@
-def table(x):
+def tafel(x):
     for i in range(1, 11):
         print(x, "x", i, "=", x * i)
 
 def main():
     x = int(input("Van welk getal wilt u de tafel zien? "))
-    table(x)
+    ta(x)
 
 main()
