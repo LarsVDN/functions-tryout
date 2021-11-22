ae2bb819b878c331aea9fabe41eda11c246922a0
LarsVDN <20481220+LarsVDN@users.noreply.github.com>
2021-11-22 09:09:53 +0100

    niks

--- fibonacci.py
+++ fibonacci.py
@@ -6,8 +6,4 @@ def fuckFibonacci(Position):
         print(fibonacci)
         previous = current
         current = fibonacci
-<<<<<<< HEAD
 fuckFibonacci(100)
-=======
-fuckFibonacci(1000000)
->>>>>>> 0ee55fee73a57e113f20b462781f187ca62dfc8a
